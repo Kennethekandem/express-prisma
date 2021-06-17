44d7b54fcc97426e48a17d72be35fba2464dc750
Kenneth Ekandem <kennyekandem@gmail.com>
2021-06-17 15:00:08 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 const user = require('../controllers/auth.controller');
 const auth = require('../middlewares/auth');
 
+// register
 router.post('/', user.register);
+
+// login
 router.post('/login', user.login);
+
+// all users
 router.get('/', auth, user.all);
 
 module.exports = router;
